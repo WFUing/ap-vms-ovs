5a5abcd98df4ec6427c5e80f8418249371e79fc3
WFUing <944427534@qq.com>
2024-05-08 12:13:20 +0800

    on hr650x

--- main.tf
+++ main.tf
@@ -18,56 +18,55 @@ provider "libvirt" {
 resource "libvirt_pool" "test_vm_storage_pool" {
   name = "test_vm-storage-pool"
   type = "dir"
-  path = "/home/wds/zhitai/test/images"
+  path = "/home/wfuing/test/images"
 }
 
 resource "libvirt_volume" "ubuntu_base" {
   name   = "ubuntu24.04-x86-64.qcow2"
-  pool   = "zhitai"
-  source = "/home/wds/zhitai/images/ubuntu24.04-x86-64.qcow2"
+  pool   = "actor"
+  source = "/home/wfuing/images/ubuntu24.04-x86-64.qcow2"
   format = "qcow2"
 }
 
-resource "null_resource" "enable_ip_forwarding" {
-  provisioner "local-exec" {
-    command = <<EOF
-sudo sysctl -w net.ipv4.ip_forward=1
-echo "net.ipv4.ip_forward = 1" | sudo tee -a /etc/sysctl.conf
-EOF
-  }
-}
+# resource "null_resource" "enable_ip_forwarding" {
+#   provisioner "local-exec" {
+#     command = <<EOF
+# sudo sysctl -w net.ipv4.ip_forward=1
+# echo "net.ipv4.ip_forward = 1" | sudo tee -a /etc/sysctl.conf
+# EOF
+#   }
+# }
 
-# 确保创建桥接和设置桥接 IP 地址的资源在启用 IP 转发之后执行
-resource "null_resource" "create_ovs_bridge" {
-  depends_on = [null_resource.enable_ip_forwarding]
-  provisioner "local-exec" {
-    command = <<EOF
-if ! sudo ovs-vsctl br-exists ovsbr0; then
-  sudo ovs-vsctl add-br ovsbr0
-fi
-EOF
-  }
+# # 确保创建桥接和设置桥接 IP 地址的资源在启用 IP 转发之后执行
+# resource "null_resource" "create_ovs_bridge" {
+#   depends_on = [null_resource.enable_ip_forwarding]
+#   provisioner "local-exec" {
+#     command = <<EOF
+# if ! sudo ovs-vsctl br-exists ovsbr0; then
+#   sudo ovs-vsctl add-br ovsbr0
+# fi
+# EOF
+#   }
 
-  provisioner "local-exec" {
-    when    = destroy
-    command = "sudo ovs-vsctl del-br ovsbr0"
-  }
-}
+#   provisioner "local-exec" {
+#     when    = destroy
+#     command = "sudo ovs-vsctl del-br ovsbr0"
+#   }
+# }
 
-resource "null_resource" "setup_bridge_ip" {
-  depends_on = [null_resource.create_ovs_bridge]
-  provisioner "local-exec" {
-    command = "sudo ip addr add 192.168.100.1/24 dev ovsbr0"
-  }
+# resource "null_resource" "setup_bridge_ip" {
+#   depends_on = [null_resource.create_ovs_bridge]
+#   provisioner "local-exec" {
+#     command = "sudo ip addr add 192.168.100.1/24 dev ovsbr0"
+#   }
 
-  provisioner "local-exec" {
-    when    = destroy
-    command = "sudo ip addr del 192.168.100.1/24 dev ovsbr0"
-  }
-}
+#   provisioner "local-exec" {
+#     when    = destroy
+#     command = "sudo ip addr del 192.168.100.1/24 dev ovsbr0"
+#   }
+# }
 
 resource "libvirt_network" "ovs_network" {
-  depends_on = [null_resource.create_ovs_bridge, null_resource.setup_bridge_ip, null_resource.enable_ip_forwarding]
   name       = "ovs-network"
   mode       = "bridge"
   bridge     = "ovsbr0"
